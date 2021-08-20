c4dafa2aed67698b2ac6bb22bbb6320d1039f565
patela23 <55360122+patela23@users.noreply.github.com>
2021-08-20 15:13:04 -0400

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){ #creates the special matrix that can cache its inverse
+  inv <- NULL 
+  set <- function(y){ #set function 
+    x<<-y
+    inv <<- NULL
+  }
+  get <-function() {x} # get function 
+  setInverse <- function(inverse) {inv <<- inverse} # set the value of inverse
+  getInverse <- function() {inv} #get the value of inverse
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
-
+  
+  #parent function is the makecachefunction 
+  #double arrows is capable of being applied to parent function as well unlike one arrow which applies to current level 
+cacheSolve <- function (x, ...){ #computes the inverseof the special "matrix" returned by makeCacheMatrix above
+  inv <-x$getInverse() #line returns a matrix that is inverse of X and assigns it to inv 
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <-x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
+  
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
